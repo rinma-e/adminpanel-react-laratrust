d6f57c60fdcaa711770784e9a1f8331cfd16b131
Marin Ercegovac <ercegovac.marin@gmail.com>
2023-11-03 15:07:33 +0100

    https on localhost - .env changes in README

--- README.md
+++ README.md
@@ -13,3 +13,13 @@ to<br/>
     'url' => $request->getBaseUrl().$request->getPathInfo()
 
 </p>
+
+<p>
+To have https work in localhost and your project accessible to all devices conected to LAN in.env:<br/>
+1. change http to https in: APP_URL=https://PROJECT_NAME.test
+2. add this lines
+    VITE_ASSET_HOST="adminboard.test"
+    VITE_ASSET_PORT=5173
+    VITE_PRIVKEY_PATH="e:/laragon/etc/ssl/laragon.key"
+    VITE_CERT_PATH="e:/laragon/etc/ssl/laragon.crt"
+</p>
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,29 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
+
+const env = loadEnv("all", process.cwd());
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: "resources/js/app.jsx",
             refresh: true,
         }),
         react(),
     ],
+    server: {
+        host: true,
+        port: env.VITE_ASSET_PORT,
+        strictPort: true,
+        hmr: {
+            host: env.VITE_ASSET_HOST,
+            port: env.VITE_ASSET_PORT,
+        },
+        https: {
+            key: fs.readFileSync(env.VITE_PRIVKEY_PATH),
+            cert: fs.readFileSync(env.VITE_CERT_PATH),
+        },
+    },
 });
