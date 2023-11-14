140dd9ec6775e65d0dd8c6f102b332c63b335281
Marin Ercegovac <ercegovac.marin@gmail.com>
2023-11-14 10:46:47 +0100

    Fix tailwind messing mantine stylings

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,8 +1,5 @@
 import "./bootstrap";
 import "../css/app.css";
-import "@mantine/core/styles.css";
-import "@mantine/dates/styles.css";
-import "@mantine/dropzone/styles.css";
 
 import { createRoot } from "react-dom/client";
 import { createInertiaApp } from "@inertiajs/react";
