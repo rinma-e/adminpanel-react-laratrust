49d1e8ba27c20fe0a19d7bd69d6245ff9c6f0686
Marin Ercegovac <ercegovac.marin@gmail.com>
2024-03-01 10:20:44 +0100

    Added mantine theme provider

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,6 +2,7 @@ import "./bootstrap";
 import "../css/app.css";
 
 import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
 import { createInertiaApp } from "@inertiajs/react";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { MantineProvider, createTheme } from "@mantine/core";
@@ -11,6 +12,16 @@ const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 const theme = createTheme({
     fontFamily: "Poppins, sans-serif",
 });
+
+function MyApp({ el, App, props }) {
+    return (
+        <StrictMode>
+            <MantineProvider theme={theme}>
+                <App {...props} />
+            </MantineProvider>
+        </StrictMode>
+    );
+}
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -22,11 +33,7 @@ createInertiaApp({
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(
-            <MantineProvider theme={theme}>
-                <App {...props} />
-            </MantineProvider>
-        );
+        root.render(<MyApp el={el} App={App} props={props} />);
     },
     progress: {
         color: "#4B5563",
