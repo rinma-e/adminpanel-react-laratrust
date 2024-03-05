8bcef8b88c89b45cdb585f33b326f9a853aca9ea
Marin Ercegovac <ercegovac.marin@gmail.com>
2024-03-05 14:53:38 +0100

    Refactored validation rules to custom UserStoreRequest

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,14 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserStoreRequest;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -29,15 +28,9 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request): RedirectResponse
+    public function store(UserStoreRequest $request): RedirectResponse
     {
-        $request->validate([
-            'first_name' => 'required|string|max:255',
-            'last_name' => 'required|string|max:255',
-            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'avatar' => 'nullable|image',
-        ]);
+        $request->validated();
 
         $user = User::create([
             'first_name' => $request->first_name,
---
+++ app/Http/Requests/UserStoreRequest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Validation\Rules\Password;
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => 'required|string|min:2|max:255',
+            'last_name' => 'required|string|min:2|max:255',
+            'email' => 'required|email:rfc,filter|unique:' . User::class,
+            'password' => ['bail', 'required', Password::defaults()],
+            'password_confirmation' => 'required',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'password_confirmation.required' => 'Please confirm your password.',
+        ];
+    }
+
+    /**
+     * Add password_confirmation validation error in case of password mismatch
+     * to be displayed on the proper form field
+     *
+     * @param \Illuminate\Validation\Validator $validator
+     * @return void
+     */
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            // if there are password errors don't proceed
+            if ($validator->errors()->has('password')) {
+                return; // Exit early if there are validation errors
+            }
+
+            // check if passwords match and add error if they don't
+            if ($this->password && $this->password_confirmation && $this->password !== $this->password_confirmation) {
+                $validator->errors()->forget('password');
+                $validator->errors()->add('password_confirmation', 'Passwords don\'t match');
+            }
+        });
+    }
+}
