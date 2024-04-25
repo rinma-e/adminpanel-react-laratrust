2de6aa1b80405a146fbed914ef882a96b99b53bc
Marin Ercegovac <ercegovac.marin@gmail.com>
2024-04-25 13:03:29 +0200

    Guest layout with Mantine components

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,11 +1,16 @@
-import ApplicationLogo from "@/Components/ApplicationLogo";
-import { Link } from "@inertiajs/react";
-import ThemeSwitcherButton from "@/Components/ThemeSwitcherButton";
-import { Center, Box, Affix } from "@mantine/core";
+import { Affix, Flex } from "@mantine/core";
+
+import { ThemeSwitcherButton } from "@/Components";
 
 export default function Guest({ children }) {
     return (
-        <Center mih={"100vh"} bg="var(--mantine-color-gray-light)">
+        <Flex
+            miw={300}
+            mih="100vh"
+            justify="center"
+            align={{ base: "flex-start", xs: "center" }}
+            bg="var(--mantine-color-gray-light)"
+        >
             <Affix
                 position={{ top: 20, right: 20 }}
                 className="keep-transition"
@@ -13,6 +18,6 @@ export default function Guest({ children }) {
                 <ThemeSwitcherButton />
             </Affix>
             {children}
-        </Center>
+        </Flex>
     );
 }
