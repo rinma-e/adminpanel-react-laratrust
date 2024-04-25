7d437c30ed6520334a3bf349f59f659bd7cb12e2
Marin Ercegovac <ercegovac.marin@gmail.com>
2024-04-25 14:15:50 +0200

    Register page with Mantine components

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -1,117 +1,215 @@
-import { useEffect } from 'react';
-import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import { Head, Link, useForm } from '@inertiajs/react';
+import { useState, useEffect } from "react";
+import { Head, router, Link } from "@inertiajs/react";
+import { useDisclosure } from "@mantine/hooks";
+import { useForm as useMantineForm } from "@mantine/form";
+import {
+    useMantineColorScheme,
+    Text,
+    Title,
+    Divider,
+    TextInput,
+    Button,
+    Anchor,
+    PasswordInput,
+    Group,
+    Stack,
+    Card,
+} from "@mantine/core";
+import {
+    IconUserPlus,
+    IconArrowBackUp,
+    IconEqual,
+    IconEqualNot,
+} from "@tabler/icons-react";
+import { zodResolver } from "mantine-form-zod-resolver";
+import GuestLayout from "@/Layouts/GuestLayout";
+import { validationSchema } from "./validationSchema";
 
 export default function Register() {
-    const { data, setData, post, processing, errors, reset } = useForm({
-        name: '',
-        email: '',
-        password: '',
-        password_confirmation: '',
+    const { colorScheme } = useMantineColorScheme();
+
+    const form = useMantineForm({
+        initialValues: {
+            first_name: "",
+            last_name: "",
+            email: "",
+            password: "",
+            password_confirmation: "",
+        },
+
+        validate: zodResolver(validationSchema),
     });
 
-    useEffect(() => {
-        return () => {
-            reset('password', 'password_confirmation');
-        };
-    }, []);
+    const [visible, { toggle }] = useDisclosure(false);
+    const [processing, setProcessing] = useState(false);
 
-    const submit = (e) => {
-        e.preventDefault();
-
-        post(route('register'));
+    const handleSubmit = (data) => {
+        router.post(route("register"), data, {
+            onProgress: () => setProcessing(true),
+            onFinish: () => setProcessing(false),
+            onError: (err) => {
+                // set server side errors in form
+                form.setErrors({ ...err });
+            },
+        });
     };
+
+    useEffect(() => {
+        if (!form.values.password) {
+            form.setFieldValue("password_confirmation", "");
+        }
+    }, [form.values.password]);
 
     return (
         <GuestLayout>
             <Head title="Register" />
 
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="name" value="Name" />
-
-                    <TextInput
-                        id="name"
-                        name="name"
-                        value={data.name}
-                        className="mt-1 block w-full"
-                        autoComplete="name"
-                        isFocused={true}
-                        onChange={(e) => setData('name', e.target.value)}
-                        required
-                    />
-
-                    <InputError message={errors.name} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="email" value="Email" />
-
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        onChange={(e) => setData('email', e.target.value)}
-                        required
-                    />
-
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={(e) => setData('password', e.target.value)}
-                        required
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password_confirmation" value="Confirm Password" />
-
-                    <TextInput
-                        id="password_confirmation"
-                        type="password"
-                        name="password_confirmation"
-                        value={data.password_confirmation}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={(e) => setData('password_confirmation', e.target.value)}
-                        required
-                    />
-
-                    <InputError message={errors.password_confirmation} className="mt-2" />
-                </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    <Link
-                        href={route('login')}
-                        className="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+            <Card withBorder w={420} p="md" shadow="xs" radius="md">
+                <Stack>
+                    <Title
+                        mx="auto"
+                        my="sm"
+                        size="3rem"
+                        c={colorScheme === "dark" ? "dark.0" : "dark.4"}
                     >
-                        Already registered?
-                    </Link>
+                        Sign up
+                    </Title>
 
-                    <PrimaryButton className="ml-4" disabled={processing}>
-                        Register
-                    </PrimaryButton>
-                </div>
-            </form>
+                    <Divider size="xs" />
+
+                    <form onSubmit={form.onSubmit(handleSubmit)}>
+                        <Stack>
+                            <TextInput
+                                label="First Name"
+                                name="first_name"
+                                autoComplete="given-name"
+                                mb="lg"
+                                size="sm"
+                                withAsterisk={false}
+                                required
+                                {...form.getInputProps("first_name")}
+                            />
+
+                            <TextInput
+                                label="Last Name"
+                                name="last_name"
+                                autoComplete="family-name"
+                                mb="lg"
+                                size="sm"
+                                withAsterisk={false}
+                                required
+                                {...form.getInputProps("last_name")}
+                            />
+
+                            <TextInput
+                                label="Email"
+                                name="email"
+                                autoComplete="email"
+                                mb="lg"
+                                size="sm"
+                                withAsterisk={false}
+                                required
+                                {...form.getInputProps("email")}
+                            />
+
+                            <PasswordInput
+                                label="Password"
+                                name="password"
+                                withAsterisk={false}
+                                visible={visible}
+                                onVisibilityChange={toggle}
+                                size="sm"
+                                mb="lg"
+                                required
+                                {...form.getInputProps("password")}
+                            />
+
+                            <PasswordInput
+                                label="Confirm Password"
+                                withAsterisk={false}
+                                visible={visible}
+                                onVisibilityChange={toggle}
+                                size="sm"
+                                mb="lg"
+                                required
+                                disabled={!form.values.password}
+                                {...form.getInputProps("password_confirmation")}
+                                leftSection={
+                                    form.values.password_confirmation &&
+                                    (form.values.password_confirmation ===
+                                    form.values.password ? (
+                                        <IconEqual
+                                            size={16}
+                                            stroke={2}
+                                            color="var(--mantine-color-green-7)"
+                                        />
+                                    ) : (
+                                        <IconEqualNot
+                                            size={16}
+                                            stroke={2}
+                                            color="var(--mantine-color-red-7)"
+                                        />
+                                    ))
+                                }
+                            />
+
+                            <Group justify="space-between">
+                                <Button
+                                    variant="outline"
+                                    leftSection={
+                                        <IconArrowBackUp
+                                            size={16}
+                                            stroke={2.5}
+                                        />
+                                    }
+                                    onClick={() =>
+                                        window.history.length > 1
+                                            ? window.history.back()
+                                            : (window.location.href = "/")
+                                    }
+                                >
+                                    Go Back
+                                </Button>
+
+                                <Button
+                                    type="submit"
+                                    disabled={processing}
+                                    leftSection={
+                                        <IconUserPlus size={16} stroke={2.5} />
+                                    }
+                                >
+                                    Sign up
+                                </Button>
+                            </Group>
+                        </Stack>
+                    </form>
+                </Stack>
+
+                <Card.Section
+                    withBorder
+                    mt="md"
+                    bg={colorScheme === "dark" ? "gray.8" : "gray.0"}
+                >
+                    <Group
+                        w="full"
+                        p="lg"
+                        align="center"
+                        justify="center"
+                        gap="xs"
+                    >
+                        <Text size="sm">Already have an account?</Text>
+
+                        <Anchor
+                            component={Link}
+                            href={route("login")}
+                            size="sm"
+                            fw={700}
+                        >
+                            Log in here
+                        </Anchor>
+                    </Group>
+                </Card.Section>
+            </Card>
         </GuestLayout>
     );
 }
