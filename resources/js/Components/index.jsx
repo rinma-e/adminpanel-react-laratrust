e194a092cbc149877481a2e5baaca51fbee6b16f
Marin Ercegovac <ercegovac.marin@gmail.com>
2024-04-25 12:55:17 +0200

    Custom components export list

---
+++ resources/js/Components/index.jsx
@@ -0,0 +1,15 @@
+import ApplicationLogo from "@/Components/ApplicationLogo/ApplicationLogo";
+import NavigationLinks from "@/Components/NavigationLinks/NavigationLinks";
+import PrimaryColorChangerButton from "@/Components/PrimaryColorChangerButton/PrimaryColorChangerButton";
+import ThemeSwitcherButton from "@/Components/ThemeSwitcherButton/ThemeSwitcherButton";
+import UserMenu from "@/Components/UserMenu/UserMenu";
+import UserButton from "@/Components/UserButton/UserButton";
+
+export {
+    ApplicationLogo,
+    NavigationLinks,
+    PrimaryColorChangerButton,
+    ThemeSwitcherButton,
+    UserMenu,
+    UserButton,
+};
