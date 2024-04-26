cbb9738ae8789a4551759e760d70abeeff2fc675
Marin Ercegovac <ercegovac.marin@gmail.com>
2024-04-26 07:43:34 +0200

    Dashboard page with Mantine components

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,21 +1,29 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import { Head } from "@inertiajs/react";
+import { Title, Text, Paper, useMantineColorScheme } from "@mantine/core";
+
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 
 export default function Dashboard({ auth }) {
+    const { colorScheme } = useMantineColorScheme();
+
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>}
+            header={
+                <Title order={2} c="grey.7" fw={300}>
+                    Dashboard
+                </Title>
+            }
         >
             <Head title="Dashboard" />
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
-                    </div>
-                </div>
-            </div>
+            <Paper
+                w="100%"
+                withBorder={colorScheme === "dark" ? true : false}
+                p="md"
+            >
+                <Text ta="left">Welcome to your dashboard.</Text>
+            </Paper>
         </AuthenticatedLayout>
     );
 }
