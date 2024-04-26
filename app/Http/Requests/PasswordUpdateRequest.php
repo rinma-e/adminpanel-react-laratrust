5cd9af0a35b6e20a84882cf8c661edf74164ac4b
Marin Ercegovac <ercegovac.marin@gmail.com>
2024-04-26 06:55:12 +0200

    Created and implemented password update request for easier validation

---
+++ app/Http/Requests/PasswordUpdateRequest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Foundation\Http\FormRequest;
+
+class PasswordUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'current_password' => ['required', 'current_password'],
+            'password' => ['required', Password::defaults()],
+            'password_confirmation' => 'required',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'password.min' => 'Password must be at least 8 characters.',
+            'password_confirmation.required' => 'Please confirm your password.',
+        ];
+    }
+
+    /**
+     * Add password_confirmation validation error in case of password mismatch
+     * to be displayed on the proper form field
+     *
+     * @param \Illuminate\Validation\Validator $validator
+     * @return void
+     */
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            // if there are password errors don't proceed
+            if ($validator->errors()->has('password')) {
+                return; // Exit early if there are validation errors
+            }
+
+            // check if passwords match and add error if they don't
+            if ($this->password && $this->password_confirmation && $this->password !== $this->password_confirmation) {
+                $validator->errors()->forget('password');
+                $validator->errors()->add('password_confirmation', 'Passwords don\'t match');
+            }
+        });
+    }
+}
