429522f886bc6e5299bc9710581cb84c1c157af1
Marin Ercegovac <ercegovac.marin@gmail.com>
2024-04-26 07:46:57 +0200

    Created protected admin-panel route

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,10 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/admin-panel', function () {
+    return Inertia::render('AdminPanel');
+})->middleware(['auth', 'verified', 'role:super-admin|admin'])->name('admin-panel');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
